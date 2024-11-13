89298b0a508e8291d1674583fa1495bd32f24be4
yonatan957 <yonatan22660@gmail.com>
2024-11-13 11:01:48 +0200

    create login and register dto

---
+++ src/DTO/loginDTO.ts
@@ -0,0 +1,5 @@
+export interface loginDTO {
+    userName: string;
+    password: string
+  }
+  
---
+++ src/DTO/registerDTO.ts
@@ -0,0 +1,7 @@
+export interface registerDTO {
+    userName: string;
+    password: string;
+    organization: string;
+    location?: string;
+  }
+  
