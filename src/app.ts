7befcb41d0daca8a1a530c843ff7a0659474fbac
yonatan957 <yonatan22660@gmail.com>
2024-11-13 12:15:45 +0200

    add cors

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import usersController from './controllers/users'
 import sidController from './controllers/sid'
 import { conectToMongo } from './config/DB'
+import cors from 'cors'
 
 const PORT = process.env.PORT
 const app = express()
@@ -10,6 +11,7 @@ const app = express()
 conectToMongo()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/sid', sidController)
 app.use('/users', usersController)
