2be195a2cc5fe235c68ef67ad2344e86354d4773
yonatan957 <yonatan22660@gmail.com>
2024-11-14 09:33:00 +0200

    add socket to update that the missle explode

--- src/services/attack.ts
+++ src/services/attack.ts
@@ -18,7 +18,7 @@ export const createAttack = async (attack:IAttack) => {
     const newAttack = await Attack.create(attack)
     return {newAttack, oranization:user.organization}
 }
-export const interceptAttack = async (interceptor:string, attack:string, user_id:string) => {
+export const interceptAttack = async (interceptor:string, attack:string, user_id:string, time:number) => {
     const user = await User.findById(user_id)
     if(!user) throw new Error('user not found')
     const weapon = user.resources.find(resource => resource.name === interceptor)
@@ -28,11 +28,23 @@ export const interceptAttack = async (interceptor:string, attack:string, user_id
     if(!attackToUpdate) throw new Error('attack not found')
     attackToUpdate.intercepted = true
     attackToUpdate.id_intercepted = user_id
+    attackToUpdate.tymeToHit = time
     await attackToUpdate.save()
     user.resources.map(resource => {
         if(resource.name === interceptor) resource.amount -= 1
         return resource
     })
     await user.save()
+    return attackToUpdate
+}
+
+export const explodeAttack = async (attack:string, user_id:string) => {
+    const attackToUpdate = await Attack.findById(attack)
+    if(!attackToUpdate) throw new Error('attack not found')
+    if(attackToUpdate.id_attacker !== user_id) throw new Error('you are not the attacker');
+    attackToUpdate.intercepted = false
+    attackToUpdate.id_intercepted = undefined
+    attackToUpdate.tymeToHit = 0
+    await attackToUpdate.save()
     return attackToUpdate
 }
--- src/socket/io.ts
+++ src/socket/io.ts
@@ -3,7 +3,7 @@ import { io } from "../app";
 import { IAttack } from "../models/Attack";
 import JWT from 'jsonwebtoken'
 import { tokenPayload } from "../DTO/tokenPayload";
-import { createAttack, interceptAttack } from "../services/attack";
+import { createAttack, explodeAttack, interceptAttack } from "../services/attack";
 
 export const handleSocketConnection = (client:Socket)=>{
     client.on('launch', async(data:{attack:IAttack, token:string})=>{
@@ -21,12 +21,17 @@ export const handleSocketConnection = (client:Socket)=>{
     client.on('goToRoom', (data:{room:string})=>{
         client.join(data.room.trim())
     })
-    client.on("intercept", async(data:{interceptor:string, token:string, attack: string})=>{
+    client.on("intercept", async(data:{interceptor:string, token:string, attack: string,time: number})=>{
         const user:tokenPayload = verifyAttack(data.token)
-        const attack = await interceptAttack(data.interceptor, data.attack, user.user_id)
-        client.emit('intercepted', attack)
+        const attack = await interceptAttack(data.interceptor, data.attack, user.user_id, data.time)
+        io.emit('intercepted', attack)
+    })
+    client.on("Attack_finished", (data:{attack:string, token:string})=>{
+        const user:tokenPayload = verifyAttack(data.token)
+        const attackToUpdate = explodeAttack(data.attack, user.user_id)
     })
 }
+
 
 const verifyAttack = (token:string)=>{
     if(!token) throw new Error('token must be provided')
