c2a52d592ed2528637e6e0df5bdb53993537adad
yonatan957 <yonatan22660@gmail.com>
2024-11-14 09:44:29 +0200

    add middleware for verify

---
+++ src/middlewares/verifyUser.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import JWT, { JsonWebTokenError } from "jsonwebtoken";
+
+export default (req:Request, res:Response, next:NextFunction)=>{
+    try {
+        const token = req.body.token
+        if(!token) {res.status(400).json({err:"token must be provided"}); return}
+        const payload = JWT.verify(token, process.env.SECRET_JWT!);
+        if (!payload) {throw new Error('you are Hacker 😱')}
+        (req as any).user = payload
+        next()
+    } catch (error) {
+        res.status(401).json(error as JsonWebTokenError)
+    }
+}
